645fc4ced4ce1ac9634d53480a7b20bb104d4154
sahil-75 <sahil.kalyani75@gmail.com>
2020-10-24 21:14:47 +0530

    Initial Commit

---
+++ LB/Reverse_The_Array.java
@@ -0,0 +1,38 @@
+import java.io.*;
+
+class Driver {
+   public static void main(String args[]) throws IOException {
+      BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+      int t = Integer.parseInt(read.readLine());
+
+      while (t-- > 0) {
+         int n = Integer.parseInt(read.readLine());
+         String str[] = read.readLine().trim().split(" ");
+
+         int input[] = new int[n];
+         for (int i = 0; i < n; i++) {
+            input[i] = Integer.parseInt(str[i]);
+         }
+
+         // int x = Integer.parseInt(read.readLine());
+         Get obj = new Get();
+         obj.reverseArray(input, n);
+
+         for (int i = 0; i < n; i++)
+            System.out.print(input[i] + " ");
+
+         System.out.println();
+      }
+   }
+}
+
+class Get {
+   public void reverseArray(int arr[], int n) throws IOException {
+      int i, t;
+      for (i = 0; i < n / 2; i++) {
+         t = arr[i];
+         arr[i] = arr[n - i - 1];
+         arr[n - i - 1] = t;
+      }
+   }
+}
